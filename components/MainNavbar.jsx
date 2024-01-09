2691b3365984f029691cee483e98e785c91a1764
daniherreraa <herreraacevedodl@gmail.com>
2024-01-09 14:05:25 -0500

    Add Sheet component to MainNavbar

--- components/MainNavbar.jsx
+++ components/MainNavbar.jsx
@@ -1,19 +1,36 @@
-import Link from "next/link"
+import { BsList } from "react-icons/bs";
+import Link from "next/link";
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger
+} from "@/components/ui/sheet";
 
 const MainNavbar = () => {
   return (
-    <div className="fixed w-full h-30 flex px-4 py-2 justify-between text-[#D8D9C5] z-50">
-        <div className="brandContainer">
-            <h4 className="brandSolaris font-semibold text-lg">Solaris</h4>
-        </div>
-        <div id="linksContainer">
-            <ul className="flex gap-8">
-                <li className="font-bold"><Link href="/">Home</Link></li>
-                <li className="font-bold"><Link href="/dashboard">App</Link></li>
-            </ul>
-        </div>
+    <div className="fixed w-full h-30 flex px-4 py-2 justify-between text-onBackground z-50">
+      <div className="brandContainer">
+        <h4 className="brandSolaris font-semibold text-lg">Solaris</h4>
+      </div>
+      <Sheet>
+        <SheetTrigger className="text-2xl">
+          <BsList />
+        </SheetTrigger>
+        <SheetContent>
+          <SheetHeader>
+            <SheetTitle>Are you absolutely sure?</SheetTitle>
+            <SheetDescription>
+              This action cannot be undone. This will permanently delete your
+              account and remove your data from our servers.
+            </SheetDescription>
+          </SheetHeader>
+        </SheetContent>
+      </Sheet>
     </div>
-  )
-}
+  );
+};
 
-export default MainNavbar
+export default MainNavbar;
